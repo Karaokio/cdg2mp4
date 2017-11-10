d34546e183780c55cf9f35ee15d0c093265fa397
Adam Buga <awbuga@gmail.com>
2017-11-10 14:29:21 -0800

    initial Flask App deployment files for Heroku

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Karaokio - CDG~2~MP4 Conversion</h1>
+    <p>It is currently {time}.</p>
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
