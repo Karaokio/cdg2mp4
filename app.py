429a453bd720163dbf9d5e0cdfe82dee4b730e02
Adam Buga <awbuga@gmail.com>
2017-11-10 19:28:27 -0800

    updating home page styling

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask_uploads import ARCHIVES, UploadSet
 from raven.contrib.flask import Sentry
 
 from datetime import datetime
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static')
+
 sentry = Sentry(app) # dsn=SENTRY_DSN (env)
 
 ## Sentry Notes:
@@ -20,12 +22,7 @@ sentry = Sentry(app) # dsn=SENTRY_DSN (env)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Karaokio - CDG~2~MP4 Conversion</h1>
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
+    return render_template('index.html')
 
 @app.route('/test')
 def testing():
