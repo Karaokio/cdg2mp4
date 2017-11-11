212ba8c61e51f943d810774b4920c403880ec31a
Adam Buga <awbuga@gmail.com>
2017-11-11 12:06:54 -0800

    adding drag-and-drop file uploader

---
+++ static/dndfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Loading drag and drop...");
+
+function readURL(input) {
+  // read the url, grab the filename, display in box
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var file_data = e.target.result;
+      var file_name = input.files[0].name;
+      input.setAttribute("data-title", "Selected: " + file_name);
+      //console.log(e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
