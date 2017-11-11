12cc12f9c4069d8a4dc679f437147ee5ed7c777d
Adam Buga <awbuga@gmail.com>
2017-11-10 21:43:46 -0800

    adding some file upload interfacing

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, g, render_template
-
-from flask_uploads import ARCHIVES, UploadSet
+from werkzeug.utils import secure_filename
+from flask_uploads import UploadSet, patch_request_class, configure_uploads
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 from raven.contrib.flask import Sentry
+
+import os
 
 from datetime import datetime
 
 app = Flask(__name__, static_url_path='/static')
+
+#app.config.from_object('yourapplication.default_settings')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cdg_key_secret_time')
+
+
+ZIPS = ('zip')
+print(os.getcwd())
+app.config['UPLOADED_ZIPS_DEST'] = 'media'
+zips = UploadSet('zips', ZIPS)
+configure_uploads(app, zips)
+patch_request_class(app, 20 * 1024 * 1024)
 
 sentry = Sentry(app) # dsn=SENTRY_DSN (env)
 
@@ -20,18 +36,21 @@ sentry = Sentry(app) # dsn=SENTRY_DSN (env)
 # 2) or generic Message:
 # sentry.captureMessage('hello, world!')
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
 
-@app.route('/test')
-def testing():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Karaokio - CDG~2~MP4 Conversion</h1>
-    <p>Testing</p>"""
+class UploadForm(FlaskForm):
+    cdg_zip = FileField(validators=[FileAllowed(zips, u'Zip files only!'), FileRequired(u'File was empty!')])
+    submit = SubmitField(u'Upload')
 
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = zips.save(form.cdg_zip.data)
+        file_url = zips.url(filename)
+    else:
+        file_url = None
+    return render_template('index.html', form=form, file_url=file_url)
 
 # in the case of a 500-error, bring up a Sentry Error Reporting Dialog
 @app.errorhandler(500)
