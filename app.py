59bbe66e86524853316bd100347d253c672fba21
Adam Buga <awbuga@gmail.com>
2017-11-17 17:40:33 -0800

    adding initial Celery integration

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, g, render_template, redirect, url_for
+#Flask Webframework
+from flask import Flask, g, render_template, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from flask_uploads import UploadSet, patch_request_class, configure_uploads
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+#Celery - Long Running Tasks
+from celery import Celery
+
+## KaraokeConverter (FFMPEG Functionality)
+from ffmpeg_wrapper import KaraokeConverter
+
 #Monitoring
 from raven.contrib.flask import Sentry
 
-import os
+#Other
+import os, time, random
 import secrets
 
 
@@ -17,14 +25,20 @@ app = Flask(__name__, static_url_path='/static')
 #app.config.from_object('yourapplication.default_settings')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cdg_key_secret_time')
 
-
+# File Uploads Settings
 ZIPS = ('zip')
-print(os.getcwd())
 app.config['UPLOADED_ZIPS_DEST'] = 'media'
 zips = UploadSet('zips', ZIPS)
 configure_uploads(app, zips)
 patch_request_class(app, 20 * 1024 * 1024)
 
+# Celery Config
+app.config['CELERY_BROKER_URL'] = os.environ.get('CELERY_BROKER_URL', 'amqp://') #redis?
+app.config['CELERY_RESULT_BACKEND'] = os.environ.get('CELERY_RESULT_BACKEND', 'amqp') #redis?
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+#Monitoring
 sentry = Sentry(app) # dsn=SENTRY_DSN (env)
 
 ## Sentry Notes:
@@ -58,9 +72,42 @@ def upload():
 def show_fileset(fileset_id):
     # show the post with the given id, the id is an integer
 
+    # Kick off Celery Task
+    task = process_files.apply_async()
+    print(task)
+    print(task.id)
 
+    return render_template('video_detail.html', fileset_id=fileset_id, task_id=task.id)
 
-    return render_template('video_detail.html', fileset_id=fileset_id)
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = process_files.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
 
 # in the case of a 500-error, bring up a Sentry Error Reporting Dialog
 @app.errorhandler(500)
@@ -69,6 +116,50 @@ def internal_server_error(error):
         event_id=g.sentry_event_id,
         public_dsn=sentry.client.get_public_dsn('https')
     )
+
+@app.route('/test/task', methods=['GET'])
+def test_task():
+    result = sum.delay(23, 42)
+    result.wait()
+    return jsonify(result.result)
+
+@app.route('/test/ffmpeg', methods=['GET'])
+def test_ffmpeg():
+    result = test_ffmpeg.delay()
+    result.wait()
+    return jsonify(result.result)
+
+
+# Celery Tasks
+@celery.task()
+def sum(a, b):
+    return a + b
+
+@celery.task()
+def test_ffmpeg():
+    if not KaraokeConverter().check_ffmpeg():
+        return "ffmpeg error."
+
+    return "ffmpeg ready."
+
+@celery.task(bind=True)
+def process_files(self):
+    """Background task that runs a long function with status updatws."""
+    verb = ['Starting up', 'Checking Files', 'Unzipping', 'Converting', 'Finalizing']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0}...'.format(random.choice(verb))
+        print("updating... message")
+        # TODO: Call Karaokio
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- static/progressbar.js
+++ static/progressbar.js
@@ -9,4 +9,53 @@ var options = {
 var nanobar = new Nanobar(options);
 
 // move bar
-nanobar.go(30); // size bar 30%
+//nanobar.go(30); // size bar 30%
+
+var status_div = document.getElementById('status');
+var task_id = status_div.dataset.taskid;
+var status_url = '/status/' + task_id;
+
+console.log(status_url);
+
+update_progress(status_url, nanobar, status_div);
+
+function update_progress(status_url, nanobar, status_div) {
+    // send GET request to status URL
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', status_url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+
+            console.log(data);
+
+            var percent = parseInt(data['current'] * 100 / data['total']);
+            nanobar.go(percent);
+            // TODO adjust childNodes
+            status_div.textContent = percent + '%';
+
+            if (data['state'] != 'PENDING' && data['state'] != 'PROGRESS') {
+                // if ('result' in data) {
+                //     // show result
+                //     $(status_div.childNodes[3]).text('Result: ' + data['result']);
+                // }
+                // else {
+                //     // something unexpected happened
+                //     $(status_div.childNodes[3]).text('Result: ' + data['state']);
+                // }
+
+                //TODO: Upon Completion Display Converted video and options to DL.
+                //TODO: refer to Karaokio.com or Youtube Uploader
+                //TODO: refer to karaokio-version for more songs!
+            }
+            else {
+                // rerun in 2 seconds
+                setTimeout(function() {
+                    update_progress(status_url, nanobar, status_div);
+                }, 2000);
+            }
+        }
+    };
+    xhr.send();
+
+}
